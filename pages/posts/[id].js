7bbd5ca7b473e10fd707c1382f5f15ebed85cb49
hendraaagil <hendraaagil@gmail.com>
2020-10-30 09:22:20 +0700

    Polishing the Post Page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,15 +1,22 @@
+import Head from 'next/head';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import Layout from '../../components/layout';
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
 
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHTML }} />
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHTML }} />
+      </article>
     </Layout>
   );
 }
